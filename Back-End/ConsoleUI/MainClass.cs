9932687269c52ce07b0015ef30dc1d3fa86766d5
E-Yoklama <117041202+E-Yoklama@users.noreply.github.com>
2022-11-06 23:37:03 +0300

    Back-End \ C#

---
+++ Back-End/ConsoleUI/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
