b86adb094d68f1075dd73cd429b7dd51037a10b2
E-Yoklama <117041202+E-Yoklama@users.noreply.github.com>
2022-11-06 23:16:59 +0300

    Add files via upload
    Backend \ C#

---
+++ E-Yoklama/Entities/Egitmen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Egitmen : IEntity
+    {
+        public int EgitmenId { get; set; }
+        public string EgitmenAdi { get; set; }
+        public string EgitmenSoyadi { get; set; }
+    }
+}
