1b75548d86cf78afdc3307f03f06b7672709c332
E-Yoklama <117041202+E-Yoklama@users.noreply.github.com>
2022-11-14 00:10:58 +0300

    Add files via upload

---
+++ Back-End/ConsoleUI/MainClass.cs
@@ -0,0 +1,24 @@
+﻿using Business;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            DersManager dersManager = new DersManager(new DersDal());
+            foreach (var ders in dersManager.GetAll())
+            {
+                Console.WriteLine(ders.DersAdi);
+            }
+
+            OgrenciManager ogrenciManager = new OgrenciManager(new OgrenciDal());
+            foreach (var ogrenci in ogrenciManager.GetAll())
+            {
+                Console.WriteLine(ogrenci.OgrenciAdi);
+            }
+        }
+    }
+}
